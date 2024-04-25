c731debbbd7cc2abe86a0d912e149536307d635d
minelsulun <118488899+minelsulun@users.noreply.github.com>
2024-04-26 01:26:47 +0300

    Geometric Shape Hierarchy
    The Shape abstract class should be created to be inherited by TwoDimensionalShape and ThreeDimensionalShape classes with the following specifications:
    
    In the Shape abstract class:
    Implement a toString() method that returns the name of the respective object (e.g., Circle, Square, etc.).
    Customize getArea() methods for each subclass.
    Implement the IComparable interface in the Shape class to enable sorting of class instances within an array based on their areas.
    In the TwoDimensionalShape class:
    Implement a getArea() method.
    In the ThreeDimensionalShape class:
    Implement getArea() and getVolume() methods.
    To achieve these features, the class hierarchy and implementations should be designed accordingly, and the Main() method should be structured to demonstrate and utilize these functionalities.
    
    Here's a summary of the tasks to be accomplished:
    
    Define an abstract class Shape with necessary methods (toString(), getArea()) and implement the IComparable interface.
    Create concrete subclasses (TwoDimensionalShape and ThreeDimensionalShape) that inherit from Shape and override the required methods (getArea(), getVolume() for ThreeDimensionalShape).
    Implement the sorting functionality using IComparable interface to compare Shape instances based on their areas.
    Design the Main() method to demonstrate the use of these classes and their functionalities, including sorting Shape instances by their areas.

--- Program.cs
+++ Program.cs
@@ -4,34 +4,226 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exceptionhandling
+namespace ornek_sekiller
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //exception = errors that occur during execution
-            //catch   = catches and handles exeptions when they occur
-            //finally = always executes regardless if exception is caught or not
-            double x, y, result;
-            try
-            {
-                Console.Write("enter number 1:");
-                x = Convert.ToDouble(Console.ReadLine());
-                Console.Write("enter number 2:");
-                y = Convert.ToDouble(Console.ReadLine());
+            Circle circle = new Circle();
+            Square square = new Square();
+            Triangle triangle = new Triangle(3,4);
 
-                result = x / y;
+            Sphere sphere = new Sphere();
+            Cube cube = new Cube();
+            Tetrahedron tetrahedron = new Tetrahedron();
 
-                Console.WriteLine("result:" + result);
+            circle.R = 6;
+            circle.toString();
+            Console.WriteLine($"area={circle.getArea()}\n");
 
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine("enter only numbers!!!");
-            }
-            Console.ReadKey();
+            square.Edge = 6;
+            square.toString();
+            Console.WriteLine($"area={square.getArea()}\n");
 
+            triangle.toString();
+            Console.WriteLine($"area={triangle.getArea()}\n\n");
+
+            sphere.R = 6;
+            sphere.toString();
+            Console.WriteLine($"area={sphere.getArea()} volume={sphere.getVolume()}\n");
+
+            cube.Edge = 6;
+            cube.toString();
+            Console.WriteLine($"area={cube.getArea()}  volume={cube.getVolume()}\n");
+
+            tetrahedron.T = 6;
+            tetrahedron.toString();
+            Console.WriteLine($"area={tetrahedron.getArea()}  volume={tetrahedron.getVolume()}\n");
+
+
+            Console.ReadLine();
         }
     }
+}
+
+
+abstract class Shape
+{
+    public abstract void toString();
+    public abstract decimal getArea();
+}
+
+class TwoDimensionalShape : Shape
+{
+    public override void toString() { }
+    public override decimal getArea() { return 0; }
+}
+
+    class Circle: TwoDimensionalShape
+    {
+        private decimal r;
+        public decimal R
+        {
+
+            get { return r; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("r cannot be negative");
+                }
+                r = value;
+            }
+        }
+        public override decimal getArea()
+        {
+            return (decimal)Math.PI * r * r;
+        }
+
+    public override void toString()
+        {
+            Console.WriteLine("its a circle ");
+        }
+    }
+    class Square : TwoDimensionalShape
+    {
+        private decimal edge;
+        public decimal Edge
+        {
+            get { return edge; }
+            set
+            {
+                if (edge < 0)
+                {
+                    throw new Exception("edge cannot be negative");
+                }
+                edge = value;
+            }
+        }
+        
+        public override void toString()
+            {
+                Console.WriteLine("its a square ");
+            }
+        public override decimal getArea()
+        {
+            return edge*edge;
+        }
+    }
+    class Triangle : TwoDimensionalShape
+    {
+        private decimal baseLength;
+        private decimal height;
+    //constructor method
+    public Triangle(decimal baseLength, decimal height)
+        {
+            this.baseLength = baseLength;
+            this.height = height;
+        }
+        public override decimal getArea()
+        {
+             
+            return baseLength * height * (decimal)(0.5); ;
+        }
+
+        public override void toString()
+        {
+            Console.WriteLine("its a triangle ");
+        }
+    }
+class ThreeDimensionalShape : Shape
+{
+    public override void toString() { }
+    public virtual decimal getVolume()
+    {
+        return 0;
+    }
+    public override decimal getArea() { return 0; }
+}
+
+    class Sphere: ThreeDimensionalShape
+    {
+        private decimal r;
+        public decimal R {  
+        
+            get { return r; }
+    
+            set 
+            { 
+            if (value<0) 
+                {
+                    throw new Exception("r cannot be negative");
+                }
+            r = value;
+            }
+        }  
+        public override void toString()
+        {
+            Console.WriteLine("its a Sphere ");
+        }
+        public override decimal getVolume()
+        {
+            return (4/3) * (decimal)Math.PI * r * r * r;
+        }
+    public override decimal getArea()
+    {
+        return 4 * (decimal)Math.PI * r * r;
+    }
+}
+    class Cube : ThreeDimensionalShape
+    {
+        private decimal edge;
+        public decimal Edge { 
+            get { return edge; } 
+            set
+            {
+            if (edge < 0)
+            {
+                throw new Exception("edge cannot be negative");
+            }
+            edge = value;
+            }
+        }
+        public override void toString()
+        {
+            Console.WriteLine("its a cube ");
+        }
+    public override decimal getVolume()
+    {
+        return edge*edge*edge;
+    }
+    public override decimal getArea()
+    {
+        return 6 * edge * edge;
+    }
+
+}
+    class Tetrahedron : ThreeDimensionalShape
+    {
+        
+    private decimal t;
+    public decimal T
+        {
+        get { return t; }
+        set { 
+            if (t < 0) 
+            { throw new Exception("t cannot be negative");} 
+            t = value;}
+        }        
+        public override void toString()
+        {
+            Console.WriteLine("its a tetrahedron ");
+        }
+        public override decimal getVolume()
+        {
+        return (t * t * t) / (6 * (decimal)Math.Sqrt(2));
+    }
+
+    public override decimal getArea()
+    {
+        return (decimal)Math.Sqrt(3) * t * t;
+    }
+
 }
